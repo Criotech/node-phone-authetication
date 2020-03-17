3909d9a1204c1f88a830fca9a26d6bac51f2523f
Criotech <opesiyanbola8991@gmail.com>
2020-03-17 11:58:48 +0100

    first commit

---
+++ config.js
@@ -0,0 +1,5 @@
+modeule.exports = {
+    serviceID: "VA3bfcbf154c9bfb681daa172cc7f4d670",
+    accountSID: "AC0d6efde29976c47528b8e6e36bd6a5a7",
+    authToken: "89707c769a4a32f889cd63d7a92de0ab"
+}
