f8fa20d6ef445f4a32827c056560faeab6f672ce
Criotech <opesiyanbola8991@gmail.com>
2020-03-17 12:02:33 +0100

    first commit

--- config.js
+++
@@ -1,5 +0,0 @@
-modeule.exports = {
-    serviceID: "VA3bfcbf154c9bfb681daa172cc7f4d670",
-    accountSID: "AC0d6efde29976c47528b8e6e36bd6a5a7",
-    authToken: "89707c769a4a32f889cd63d7a92de0ab"
-}
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 
 const port = 3000;
 
+
+
 app.listen(port, () => {
     console.log(`Server is running at ${port}`)
 })
